63a2c73c9bacc42b650ee234004385a0539fae44
Daniel Godin <DanielEGodin@gmail.com>
2026-03-29 11:27:35 -0700

    basics set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-
+import styles from './App.module.css';
 
 export function App() {
 
+    const runGame = () => {
+        console.log('game started');
+    }
+
     return (
-        <></>
+        <div className={styles.app}>
+            <h1>Flip 7 Game / Simulation</h1>
+
+            <button type='button' onClick={runGame}>Run Game</button>
+
+        </div>
     )
 }
